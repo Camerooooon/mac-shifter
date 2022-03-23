9732fdb15f9497aa1258b1e746621384502e6f09
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-03-22 19:41:34 -0700

    Add new macs

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -21,3 +21,34 @@ def test_new_mac():
 
     assert mac[0] == mac[1] == "F"
     assert mac[3] == mac[4] == "1"
+    assert mac[2] == mac[5] == ":"
+
+
+def test_new_mac_dashes():
+    class NewMac(gen.BaseMac):
+        template = "FF:11:##:##:##:##"
+
+    new_mac = NewMac(dashes=True)
+    mac = new_mac.mac
+
+    assert mac[0] == mac[1] == "F"
+    assert mac[3] == mac[4] == "1"
+    assert mac[2] == mac[5] == "-"
+
+
+def test_mac_google():
+    new_mac = gen.Google()
+    a, b, c, _, _, _ = new_mac.mac.split(":")
+
+    assert a == "D8"
+    assert b == "EB"
+    assert c == "46"
+
+
+def test_mac_apple():
+    new_mac = gen.Apple()
+    a, b, c, _, _, _ = new_mac.mac.split(":")
+
+    assert a == "60"
+    assert b == "8B"
+    assert c == "0E"
