129e76c976ce6aaad33c012d855d624434b52b23
Camerooooon <cjpuffnstuff@gmail.com>
2022-03-24 21:21:43 -0700

    Basic fuzzy search implementtion

--- mac/reader.py
+++ mac/reader.py
@@ -1,5 +1,10 @@
 """Functions related to getting information based on mac address"""
-
+import parser as parser
+from thefuzz import process
 
 def get_name_of_mac(mac: str) -> str:
     pass
+
+def get_search_results(query: str) -> []:
+    to_search = parser.parse(parser.get_mac_file()).values()
+    return process.extract(query, to_search, limit=5)
