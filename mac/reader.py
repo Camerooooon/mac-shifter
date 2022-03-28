0dfd5b3ad477da40076c856252087b0ef02dfb84
Camerooooon <cjpuffnstuff@gmail.com>
2022-03-28 14:22:48 -0700

    Formatting

--- mac/reader.py
+++ mac/reader.py
@@ -2,16 +2,19 @@
 from mac import parser as parser
 from thefuzz import process
 
+
 def get_name_of_mac(mac: str) -> str:
     pass
+
 
 def get_mac(name: str) -> str:
     # We need to improve lookup times this is very slow!
     mac_file = parser.parse(parser.get_mac_file())
     for mac, company in mac_file.items():
         if company == name:
-            return mac 
+            return mac
     return "AA:AA:AA:AA:AA"
+
 
 def get_search_results(query: str) -> []:
     to_search = parser.parse(parser.get_mac_file()).values()
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import argparse
 
 from mac import reader as reader
 
+
 def parser():
     parse = argparse.ArgumentParser()
     parse.add_argument("-o", "--option", help="Option")
 
     # Search Parsing
     subparsers = parse.add_subparsers(dest="search")
-    subparser = subparsers.add_parser('search')
-    subparser.add_argument('search', nargs='+', help='search for a mac address')
+    subparser = subparsers.add_parser("search")
+    subparser.add_argument("search", nargs="+", help="search for a mac address")
     return parse.parse_args()
 
 
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     args = parser()
 
     if args.search:
-        search = " ".join(args.search);
+        search = " ".join(args.search)
         print("Searching for: " + search)
         results = reader.get_search_results(search)
         index = 0
@@ -27,9 +28,8 @@ if __name__ == "__main__":
             index += 1
 
         selection = input("Which one? #")
-        selected_company = results[int(selection)][0]  
+        selected_company = results[int(selection)][0]
         print(selected_company + " -> " + reader.get_mac(selected_company))
-
 
     if args.option:
         print("Option!")
